a3ad0a0ebc76c7bf99f4cb216576d005d3bf3eff
algos-soft <gac@algos.it>
2021-07-15 20:24:59 +0200

    fix banner

--- src/main/java/com/algos/flexo/Application.java
+++ src/main/java/com/algos/flexo/Application.java
@@ -1,12 +1,12 @@
 package com.algos.flexo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
-//import org.vaadin.artur.helpers.LaunchUtil;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.web.servlet.support.*;
+import org.springframework.scheduling.annotation.*;
+import org.vaadin.artur.helpers.*;
 
-import javax.annotation.PostConstruct;
+import javax.annotation.*;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,32 +15,33 @@ import javax.annotation.PostConstruct;
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
 
-    public static final String APP_NAME="Flexo";
+    public static final String APP_NAME = "Flexo";
 
     // the resource for the generic index icon
-    public static final String GENERIC_INDEX_ICON="images/generic_index.jpg";
+    public static final String GENERIC_INDEX_ICON = "images/generic_index.jpg";
 
     // the file containing all the potentially available symbols
-    public static final String ALL_AVAILABLE_SYMBOLS="config/indexes.csv";
+    public static final String ALL_AVAILABLE_SYMBOLS = "config/indexes.csv";
 
     // the file containing all the etoro symbols
-    public static final String ETORO_INSTRUMENTS="config/etoro_instruments.csv";
+    public static final String ETORO_INSTRUMENTS = "config/etoro_instruments.csv";
 
     // size of a stored icon on the database (the icon is always resized before storage)
-    public static final int STORED_ICON_WIDTH=128;
-    public static final int STORED_ICON_HEIGHT =128;
+    public static final int STORED_ICON_WIDTH = 128;
+
+    public static final int STORED_ICON_HEIGHT = 128;
+
+
+    public static void main(String[] args) {
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(com.algos.flexo.Application.class, args));
+    }
+
 
     @PostConstruct
     void started() {
         // set JVM timezone as UTC
         //TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
-
-    public static void main(String[] args) {
-//        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(com.algos.stockscanner.Application.class, args));
-        SpringApplication.run(com.algos.flexo.Application.class, args);
-    }
-
 
 
 }
