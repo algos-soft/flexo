2dad8d9e6253171462fc54cd88eaf608b8907b14
algos-soft <algos12>
2021-07-30 19:09:40 +0200

    .

--- src/main/java/com/algos/flexo/Application.java
+++ src/main/java/com/algos/flexo/Application.java
@@ -32,7 +32,8 @@ public class Application extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
-        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        //LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        SpringApplication.run(com.algos.flexo.Application.class, args);
     }
 
 
